6fdbd02f0702ede96ea40be92e2b365047c92e17
Demo User <debian@beaglebone.localdomain>
2020-05-11 13:33:42 -0700

    fix babystep

--- web_dwc2.py
+++ web_dwc2.py
@@ -1116,8 +1116,8 @@ class web_dwc2:
 			return None
 
 		mm_step = gcmd.get_float('Z', None)
-		if not mm_step: mm_step = self.get_float('S', None)	#	DWC 1 workarround
-		gcmd2 = self.parse_params('SET_GCODE_OFFSET Z_ADJUST' + str(mm_step) + ' MOVE1')
+		if not mm_step: mm_step = gcmd.get_float('S', None)	#	DWC 1 workarround
+		gcmd2 = self.parse_params('SET_GCODE_OFFSET Z_ADJUST=' + str(mm_step) + ' MOVE1')
 		self.gcode.cmd_SET_GCODE_OFFSET(gcmd2)
 		self.gcode_reply.append('Z adjusted by %0.2f' % mm_step)
 
